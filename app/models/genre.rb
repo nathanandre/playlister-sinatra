26c376e661fb802aaad263521df88be872e842c7
nathanandre <nathanandreburns@gmail.com>
2021-11-16 21:32:00 -0500

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,8 +2,9 @@ class Genre < ActiveRecord::Base
     has_many :song_genres 
     has_many :artists, through: :songs 
     has_many :songs, through: :song_genres 
+
+    def slug
+        name.downcase.tr(' ', '-')
+    end
 end 
 
-def slug
-    name.downcase.tr(' ', '-')
-end
